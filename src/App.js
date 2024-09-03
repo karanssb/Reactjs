46bf6ad8c52da7f0c47f2e46f507dc0b408d25df
karanssb <karanssb@gmail.com>
2024-09-02 23:13:09 -0400

    Node js project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello karan, React! This is my first React app.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
